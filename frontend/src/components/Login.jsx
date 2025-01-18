90347623ef599431f316b685168b608a49d26a47
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2025-01-18 23:51:44 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = ({setIsAuthenticated}) => {
     }
     const handleSubmit=(e)=>{
         e.preventDefault();
-        axios.post("http://localhost:3000/api/v1/user/signin",formData)
+        axios.post("https://full-stack-todolist.onrender.com/api/v1/user/signin",formData)
         .then((response)=>{
 
             localStorage.setItem('token',response.data.token);
